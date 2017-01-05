3640cd3fa54c3c4ee82100e3c08a437f35059431
Arthur Hsu <arthurhsu@chromium.org>
2017-01-05 14:41:37 -0800

    Add missing file

---
+++ lib/schema/predicate_holder.ts
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * Copyright 2017 The Lovefield Project Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {BindableValueHolder} from './bindable_value_holder';
+import {OperandType} from '../spec/predicate';
+import {ColumnSchema} from './column_schema';
+
+export abstract class PredicateHolder {
+  abstract toSql(): string;
+
+  static eval(value: OperandType): string {
+    if (value instanceof ColumnSchema) {
+      return (value as ColumnSchema).canonicalName;
+    }
+
+    if (value instanceof BindableValueHolder) {
+      return value.toString();
+    }
+
+    return BindableValueHolder.format(value);
+  };
+}
+
+export class UnaryPredicateHolder extends PredicateHolder {
+  private sql: string;
+  constructor(column: ColumnSchema, sql: string) {
+    super();
+    this.sql = `${column.canonicalName} ${sql}`;
+  }
+
+  public toSql(): string {
+    return this.sql;
+  }
+}
+
+export class BinaryPredicateHolder extends PredicateHolder {
+  constructor(readonly column: ColumnSchema, readonly sql: string,
+              readonly value: OperandType) {
+    super();
+  }
+
+  public toSql(): string {
+    return `${this.column.canonicalName} ${this.sql} ` +
+           `${PredicateHolder.eval(this.value)}`;
+  }
+}
